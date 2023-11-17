d04cd6738288473d7fc0faaf154ef8327485fd50
Victor Nkwuo <vobinn3@gmail.com>
2023-11-17 17:29:03 +0300

    updated

--- server.js
+++ server.js
@@ -8,10 +8,9 @@ app.set('view engine', 'ejs');
 app.set('views',  'views');
 
 
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define a route
+
 app.get('/', (req, res) => {
     res.render('home');
    
@@ -22,7 +21,6 @@ app.get('/portfolio', (req, res) => {
  
 });
 
-// Start the serverss
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
